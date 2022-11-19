45724485e8ac1638c81d3678dbda9f78dfc2bb47
flopitt90 <flopitta_90@hotmail.com>
2022-11-19 12:25:15 +0000

    meta changes

--- src/components/AboutFlorencia.jsx
+++ src/components/AboutFlorencia.jsx
@@ -50,7 +50,7 @@ export const AboutFlorencia = () => {
           </IconsDiv>
           </Container>
        </div>
-       <img width='400px' src='https://scontent.fsvq2-1.fna.fbcdn.net/v/t31.18172-8/16665941_10212363017616319_470226629075681926_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=XjAcH73Fmm0AX8hj83t&_nc_ht=scontent.fsvq2-1.fna&oh=00_AfBB_vv4Vkbc0GTPwgx2om2gcIb9Ncgic2qPwIQHK4lgTQ&oe=63977063'/>
+       <img width='400px' alt='Florencia' src='https://scontent.fsvq2-1.fna.fbcdn.net/v/t31.18172-8/16665941_10212363017616319_470226629075681926_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=XjAcH73Fmm0AX8hj83t&_nc_ht=scontent.fsvq2-1.fna&oh=00_AfBB_vv4Vkbc0GTPwgx2om2gcIb9Ncgic2qPwIQHK4lgTQ&oe=63977063'/>
     </DivDetail>
     <Volver onClick={ backToHome }>volver</Volver>
  </div>
